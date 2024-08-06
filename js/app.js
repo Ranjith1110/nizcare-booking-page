b3a2dd31b5c0019ea8f2b2167b34450844fab0f6
Ranjith Natarajan <132538605+Ranjith1110@users.noreply.github.com>
2024-08-06 20:55:47 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,29 @@
 
-// document.getElementById('menu-toggle').addEventListener('click', function () {
-//     document.getElementById('sidebar').classList.toggle('sidebar-hidden');
-// });
+//   Fetching data from json file start
+document.addEventListener('DOMContentLoaded', function () {
+    const selectElement = document.getElementById('corporate-json');
 
+    fetch('cities.json')
+        .then(response => response.json())
+        .then(data => {
+            const city = data.city;
+            city.forEach(corp => {
+                const option = document.createElement('option');
+                option.value = corp;
+                option.textContent = corp;
+                selectElement.appendChild(option);
+            });
+        })
+        .catch(error => console.error('Error fetching JSON:', error));
+});
+//   Fetching data from json file end
+
+//   Menubar function start
 document.getElementById('menu-toggle').addEventListener('click', function () {
     var sidebar = document.getElementById('sidebar');
     var mainContent = document.getElementById('main-content');
     sidebar.classList.toggle('sidebar-hidden');
     mainContent.classList.toggle('full-width');
 });
+//   Menubar function start
+
